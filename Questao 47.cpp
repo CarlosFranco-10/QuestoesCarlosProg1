11ab784e9414db6e0ab57c875fbfd75b93cb414e
CarlosFranco-10 <ce.sousafranco@gmail.com>
2025-07-15 23:36:36 -0300

    Update Questao 47.cpp

--- Questao 47.cpp
+++ Questao 47.cpp
@@ -8,19 +8,19 @@ int main() {
         printf("Cliente %d\n", i);
 
         printf("Digite o nome do cliente: ");
-        scanf(" %[^\n]", nome);  // lê nome com espaços
+        scanf(" %[^\n]", nome);  
 
         printf("Digite o valor das compras no ano passado: R$ ");
         scanf("%f", &compras);
 
-        // Calcula o bônus
+       
         if (compras < 500000) {
             bonus = compras * 0.10;
         } else {
             bonus = compras * 0.15;
         }
 
-        // Exibe a correspondência
+        
         printf("\n--- MENSAGEM PARA O CLIENTE ---\n");
         printf("Prezado(a) %s,\n", nome);
         printf("Com base em suas compras de R$ %.2f no ano passado,\n", compras);
