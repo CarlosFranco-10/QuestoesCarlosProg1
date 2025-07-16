f65961aa03dd36b79a3ce377933462c0e1e5194c
CarlosFranco-10 <ce.sousafranco@gmail.com>
2025-07-15 23:37:05 -0300

    Update Questao 48.cpp

--- Questao 48.cpp
+++ Questao 48.cpp
@@ -8,18 +8,18 @@ int main() {
     for (i = 1; i <= 75; i++) {
         printf("\nAluno %d\n", i);
 
-        // Leitura da matrícula
+        
         printf("Digite o número de matrícula: ");
         scanf("%d", &matricula);
 
-        // Leitura da nota final
+        
         printf("Digite a nota final do aluno (0.0 a 10.0): ");
         scanf("%f", &nota);
 
-        // Convertendo nota em uma faixa inteira (ex: 4.5 → 4, 7.8 → 7)
+        
         faixa = (int)nota;
 
-        // Mostrar o conceito final com base na faixa
+        
         printf("Matrícula: %d - Nota: %.1f - Conceito: ", matricula, nota);
 
         switch (faixa) {
