07791b3ab4794cd2217fc509725c9af531a02508
CarlosFranco-10 <ce.sousafranco@gmail.com>
2025-07-15 23:41:44 -0300

    Update Questao 53.cpp

--- Questao 53.cpp
+++ Questao 53.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Função para verificar se um número é primo (exceto 1)
+
 int ehPrimo(int num) {
     if (num < 2) return 0;
 
@@ -12,14 +12,14 @@ int ehPrimo(int num) {
 }
 
 int main() {
-    int contador = 3; // já temos 1, 2 e 3
+    int contador = 3; 
     int numero = 4;
 
-    // Imprime os três primeiros manualmente
+    
     printf("Os 20 primeiros numeros primos (incluindo 1 como pedido):\n");
     printf("1 2 3 ");
 
-    // Busca os próximos 17 números primos
+    
     while (contador < 20) {
         if (ehPrimo(numero)) {
             printf("%d ", numero);
